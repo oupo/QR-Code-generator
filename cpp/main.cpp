d82a566e80efee9eedc4374514c33272c938e024
oupo <oupo.nejiki@gmail.com>
2020-01-19 11:37:38 +0900

    update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,19 +7,47 @@
 #include <vector>
 #include "BitBuffer.hpp"
 #include "QrCode.hpp"
+#include <chrono>
 
 using std::uint8_t;
 using qrcodegen::QrCode;
 using qrcodegen::QrSegment;
 
+const int BLOCK_SIZE = 0x900;
+
+void writeRandomData(std::vector<uint8_t>& data, uint32_t seed) {
+	data.resize(BLOCK_SIZE);
+	for (int i = 0; i < BLOCK_SIZE / 4; i++) {
+		data[i * 4 + 0] = seed & 0xff;
+		data[i * 4 + 1] = (seed >> 8) & 0xff;
+		data[i * 4 + 2] = (seed >> 16) & 0xff;
+		data[i * 4 + 3] = (seed >> 24) & 0xff;
+		seed = seed * 0x41c64e6d + 0x6073;
+	}
+}
+
+QrCode dumpQR(int blockid, uint8_t* buf, int len)
+{
+	std::vector<uint8_t> vec(4 + len);
+	vec[0] = blockid & 0xff;
+	vec[1] = (blockid >> 8) & 0xff;
+	vec[2] = (blockid >> 16) & 0xff;
+	vec[3] = (blockid >> 24) & 0xff;
+	std::copy(buf, buf + len, vec.begin() + 4);
+	std::vector<QrSegment> segs{ QrSegment::makeEci(27), QrSegment::makeBytes(vec) };
+	int mask = 7;
+	return QrCode::encodeSegmentsWide(segs, mask);
+}
+
 int main() {
-	const unsigned char text[] = "Hello, world!";
-	int len = strlen((const char *)text);
-	std::vector<uint8_t> vec(len);
-	vec.assign(text, text + len);
-	
-	std::vector<QrSegment> segs{QrSegment::makeBytes(vec)};
-	const QrCode qr = QrCode::encodeSegmentsWide(segs);
-	std::cout << qr.toSvgString(4) << std::endl;
+	QrCode::initialize();
+	std::vector<uint8_t> testdata;
+	writeRandomData(testdata, 0xdeadbeef);
+	auto start = std::chrono::steady_clock::now();
+	for (int i = 0; i < 3000; i++) {
+		QrCode qr = dumpQR(-1, &testdata[0], BLOCK_SIZE);
+	}
+	auto end = std::chrono::steady_clock::now();
+	std::cerr << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1e9 << std::endl;
 	return EXIT_SUCCESS;
 }
